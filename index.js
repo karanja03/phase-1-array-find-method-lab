fbc2b9166e507ead81de8a18fddd5110c60d21a3
karanja03 <wambuik03@gmail.com>
2023-06-19 11:40:38 +0300

    Complete array find method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(array) {
+  const winningYear = array.find(obj => obj.result === "W");
+  return winningYear ? winningYear.year : undefined;
+}
+const superbowls = [
+  { year: 2000, result: "L" },
+  { year: 2001, result: "W" },
+  { year: 2002, result: "L" },
+  // ... more objects ...
+];
+
+const winningYear = superbowlWin(superbowls);
+console.log(winningYear); 
