94e6acfac5b363a6bf83d8a0948a3f7712a9365a
karanja03 <wambuik03@gmail.com>
2023-06-19 14:08:27 +0300

    complete ass

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // code your solution here
-function superbowlWin(array) {
-  const winningYear = array.find(obj => obj.result === "W");
+function superbowlWin(winningYear) {
+   winningYear = winningYear.find(obj => obj.result === "W");
   return winningYear ? winningYear.year : undefined;
 }
+
+
 const superbowls = [
+
+  
   { year: 2000, result: "L" },
   { year: 2001, result: "W" },
   { year: 2002, result: "L" },
-  // ... more objects ...
+  
 ];
 
 const winningYear = superbowlWin(superbowls);
+
 console.log(winningYear); 
